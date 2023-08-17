720a3a7f0fd0479260850c66dcbc67d17f08755d
Pri Reis <priscillareissouza@outlook.com>
2023-08-17 19:49:25 -0300

    commit inicial

---
+++ src/test/java/com/projeto/universojogos/UniversojogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.universojogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversojogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

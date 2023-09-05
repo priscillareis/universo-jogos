5bb5d7e1e4754cea991e5ffa90400e518c85941d
Pri Reis <priscillareissouza@outlook.com>
2023-09-05 10:47:15 -0300

    feat(plataforma): teste unitario request

--- src/test/java/com/projeto/universojogos/adapter/dto/PlataformaRequestTest.java
+++ src/test/java/com/projeto/universojogos/adapter/dto/PlataformaRequestTest.java
@@ -1,37 +1,135 @@
 package com.projeto.universojogos.adapter.dto;
 
-import com.projeto.universojogos.adapter.config.validation.ValidacaoCadastro;
+import com.projeto.universojogos.adapter.config.validation.*;
+import jakarta.validation.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.validation.annotation.Validated;
 
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class PlataformaRequestTest {
+    private static Validator validator;
 
-    @DisplayName("Adapter * Plataforma request correto")
-    @Validated(ValidacaoCadastro.class)
+    @BeforeAll
+    public static void setupValidatorInstance() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @DisplayName("Adapter * Deve criar a request de cadastro com sucesso")
     @Test
     public void cadastrarPlataformaComSucesso(){
         PlataformaRequest plataformaRequest = new PlataformaRequest(null, "Sony");
 
-        assertNotNull(plataformaRequest);
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoCadastro.class);
+
+        assertAll(
+                () -> assertNotNull(plataformaRequest),
+                () -> assertThat(plataformaRequest.getNome()).isEqualTo("Sony"),
+                () -> assertThat(violations.size()).isEqualTo(0)
+        );
     }
 
-    /* TODO - Construção do teste com validator factory
-
-    https://stackoverflow.com/questions/29069956/how-to-test-validation-annotations-of-a-class-using-junit
-    https://karuppiah7890.github.io/blog/posts/validating-beans-and-testing-it/
-
-    @DisplayName("Adapter * Plataforma request sem nome")
-    @Validated(ValidacaoCadastro.class)
+    @DisplayName("Adapter * Deve ter violações ao tentar criar request de cadastro sem nome")
     @Test
     public void cadastrarPlataformaComErro() {
+        PlataformaRequest plataformaRequest = new PlataformaRequest(null, "");
+
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoCadastro.class);
+
+        assertThat(violations.size()).isEqualTo(1);
+
+        violations.forEach(violation -> assertThat(violation.getPropertyPath().toString()).isEqualTo("nome"));
 
     }
 
-     */
+    @DisplayName("Adapter * Deve criar a request de consulta com sucesso")
+    @Test
+    public void consultarPlataformaComSucesso(){
+        PlataformaRequest plataformaRequest = new PlataformaRequest(5, null);
 
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoConsulta.class);
+
+        assertAll(
+                () -> assertNotNull(plataformaRequest),
+                () -> assertThat(plataformaRequest.getId()).isEqualTo(5),
+                () -> assertThat(violations.size()).isEqualTo(0)
+        );
+    }
+
+    @DisplayName("Adapter * Deve ter violações ao tentar criar request de consulta sem id")
+    @Test
+    public void consultarPlataformaComErro() {
+        PlataformaRequest plataformaRequest = new PlataformaRequest(null, "Xbox");
+
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoConsulta.class);
+
+        assertThat(violations.size()).isEqualTo(1);
+
+        violations.forEach(violation -> assertThat(violation.getPropertyPath().toString()).isEqualTo("id"));
+
+    }
+
+    @DisplayName("Adapter * Deve criar a request de atualizar com sucesso")
+    @Test
+    public void atualizarPlataformaComSucesso(){
+        PlataformaRequest plataformaRequest = new PlataformaRequest(12, "Nintendo");
+
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoAtualizacao.class);
+
+        assertAll(
+                () -> assertNotNull(plataformaRequest),
+                () -> assertThat(plataformaRequest.getId()).isEqualTo(12),
+                () -> assertThat(plataformaRequest.getNome()).isEqualTo("Nintendo"),
+                () -> assertThat(violations.size()).isEqualTo(0)
+        );
+    }
+
+    @DisplayName("Adapter * Deve ter violações ao tentar criar request de atualizar sem id e sem nome")
+    @Test
+    public void atualizarPlataformaComErro() {
+        PlataformaRequest plataformaRequest = new PlataformaRequest(null, null);
+
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoAtualizacao.class);
+
+        assertThat(violations.size()).isEqualTo(2);
+    }
+
+    @DisplayName("Adapter * Deve ter violações ao tentar criar request de atualizar sem id")
+    @Test
+    public void atualizarPlataformaComErroId() {
+        PlataformaRequest plataformaRequest = new PlataformaRequest(null, "nintendo");
+
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoAtualizacao.class);
+
+        assertThat(violations.size()).isEqualTo(1);
+
+        violations.forEach(violation -> assertThat(violation.getPropertyPath().toString()).isEqualTo("id"));
+    }
+
+    @DisplayName("Adapter * Deve ter violações ao tentar criar request de atualizar sem nome")
+    @Test
+    public void atualizarPlataformaComErroNome() {
+        PlataformaRequest plataformaRequest = new PlataformaRequest(20, "");
+
+        Set<ConstraintViolation<PlataformaRequest>> violations =  validator.validate(plataformaRequest,
+                ValidacaoAtualizacao.class);
+
+        assertThat(violations.size()).isEqualTo(1);
+
+        violations.forEach(violation -> assertThat(violation.getPropertyPath().toString()).isEqualTo("nome"));
+    }
 }
